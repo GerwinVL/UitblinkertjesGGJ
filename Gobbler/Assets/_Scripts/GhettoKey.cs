259a3af41baa0b32b39a9619be0e66eb2e2bf91a
WalterAHulsebos <treemsi@hotmail.com>
2018-01-27 02:26:49 +0100

    Gobbler

---
+++ Gobbler/Assets/_Scripts/GhettoKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhettoKey : MonoBehaviour
+{
+
+    public KeyCode key = KeyCode.Mouse0;
+
+	void Update ()
+    {
+        if (Input.GetKey(key))
+        {
+            Debug.Log("Input: " + key);
+        }
+	}
+}

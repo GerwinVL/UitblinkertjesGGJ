1e98d1062ceee595b182a0f6b824591f7b7dfd04
WalterAHulsebos <treemsi@hotmail.com>
2018-01-27 02:03:16 +0100

    Extension + GeyKeyDown Fixed

---
+++ GGJ18/Assets/_Scripts/GhettoKey.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhettoKey : MonoBehaviour
+{
+    public KeyCode click = KeyCode.Mouse0;
+
+	void Update ()
+    {
+        if (Input.GetKey(click))
+        {
+            Debug.Log("Clicked" + click);
+        }
+	}
+}

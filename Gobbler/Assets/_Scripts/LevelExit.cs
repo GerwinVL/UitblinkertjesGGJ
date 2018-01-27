460a0a5c464108f54a9ca453af2d2822de3ba7fd
Gerbor <gerjohnvandenbosch@hotmail.com>
2018-01-27 15:27:17 +0100

    Level update

--- Gobbler/Assets/_Scripts/LevelExit.cs
+++ Gobbler/Assets/_Scripts/LevelExit.cs
@@ -6,7 +6,7 @@ public class LevelExit : MonoBehaviour {
 
     public Transform ballPos;
     public Vector3 camPos;
-    public float camSpeed;
+    private float camSpeed = 5;
     private bool activated;
 
     private void OnTriggerEnter(Collider c)

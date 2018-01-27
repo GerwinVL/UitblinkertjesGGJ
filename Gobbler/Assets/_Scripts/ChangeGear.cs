98df35f20f314a0fa1ac5bcfbcab365c9c0518d7
Jannie <jan.vijfhuizen@outlook.com>
2018-01-27 13:27:09 +0000

    Rotation Fixed

---
+++ Gobbler/Assets/_Scripts/ChangeGear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeGear : MonoBehaviour {
+
+    public Gear gear;
+    private Renderer gearOutline;
+
+    private void Awake()
+    {
+        gearOutline = GetComponent<Renderer>();
+    }
+
+    public void SetColor(Color color)
+    {
+        gearOutline.material.color = color;
+    }
+}
--- Gobbler/Assets/_Scripts/Gear.cs
+++ Gobbler/Assets/_Scripts/Gear.cs
@@ -7,18 +7,13 @@ using Jext;
 public class Gear : MonoBehaviour, IComparable<Gear>
 {
     public Gear parent;
+    [HideInInspector]
+    public Gear child;
     [Range(0, 359)]
     public int angle;
     public float size;
     [HideInInspector]
     public bool right;
-    [SerializeField]
-    private Renderer gearOutline;
-
-    public void SetColor(Color color)
-    {
-        gearOutline.material.color = color;
-    }
 
     public int CompareTo(Gear other)
     {

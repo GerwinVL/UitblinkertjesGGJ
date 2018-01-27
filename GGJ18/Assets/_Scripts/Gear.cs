5fc6954562f109fee2d4df6a25f66e1034527c41
Jannie <jan.vijfhuizen@outlook.com>
2018-01-27 00:49:38 +0000

    Connect gear system finished
    EZ, your move america.

---
+++ GGJ18/Assets/_Scripts/Gear.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Jext;
+
+public class Gear : MonoBehaviour, IComparable<Gear> {
+
+    public Gear parent;
+    [Range(0, 359)]
+    public int angle;
+    public float size;
+
+    public int CompareTo(Gear other)
+    {
+        return name.Last() - other.name.Last();
+    }
+}

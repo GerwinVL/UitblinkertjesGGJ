7b60ea96f25b606768c8911458fbe6410f36c67a
Gerwin <GGGamingVL@gmail.com>
2018-01-28 14:23:03 +0100

    MainMenuPush

---
+++ Gobbler/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public gameObject mainPage;
+    public gameObject creditsPage;
+
+    public void ButtonInput(int i)
+    {
+        switch (i)
+        {
+            //Credits page
+            case 0:
+                mainPage.SetActive(false);
+                creditsPage.SetActive(false);
+                break;
+        }
+    }
+}

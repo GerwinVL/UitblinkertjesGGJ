63360c6b4697eb15f53ac3b36f9f572029f5d4e8
Jannie <jan.vijfhuizen@outlook.com>
2018-01-28 13:38:51 +0000

    Scores added

--- Gobbler/Assets/_Scripts/GameManager.cs
+++ Gobbler/Assets/_Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Jext;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -16,6 +17,8 @@ public class GameManager : MonoBehaviour {
         instance = this;
         path = Methods.GenerateFilePath(highScoreSavePath);
         highScores = Methods.Load<HighScores>(path);
+        if (highScores == null)
+            highScores = new HighScores();
     }
 
     #region Timer
@@ -26,11 +29,14 @@ public class GameManager : MonoBehaviour {
 
     private float time;
     private Coroutine timer;
+    [SerializeField]
+    private Text timerText;
     private IEnumerator Timer()
     {
         time = 0;
         while(true)
         {
+            timerText.text = ConvScore(time);
             time += Time.deltaTime;
             yield return null;
         }
@@ -60,6 +66,25 @@ public class GameManager : MonoBehaviour {
         Methods.Save(highScores, path);
     }
 
+    [SerializeField, Tooltip("5 Lang")]
+    private List<Text> scores = new List<Text>();
+    [SerializeField]
+    private GameObject scoreHolder;
+    [SerializeField]
+    private Image scoreBoard;
+    [SerializeField]
+    private float fadeSpeed;
+    private int place;
+    public IEnumerator ShowScores(float newScore)
+    {
+        place = AddScore(newScore);
+        yield return StartCoroutine(scoreBoard.FadeToBlack(fadeSpeed, Methods.FadeType.FadeOut));
+        scoreHolder.gameObject.SetActive(true);
+
+        for (int score = 0; score < 5; score++)
+            scores[score].text = place == score ? "Your score: " + "" + ConvScore(highScores.scores[score]);
+    }
+
     public class HighScores
     {
         public float[] scores = new float[5];
@@ -69,5 +94,10 @@ public class GameManager : MonoBehaviour {
             for (int score = 0; score < scores.Length; score++)
                 scores[score] = 0;
         }
+    }
+
+    private string ConvScore(float score)
+    {
+        return (Mathf.Round(score * 100f) / 100f).ToString();
     }
 }
